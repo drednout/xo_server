c921e732a99d2a1531019edf61fdc6c254c6e0b8
Alexei Romanoff <drednout.by@gmail.com>
2015-01-08 09:21:05 +0300

    Session and Player models have been basically implemented

---
+++ xo_server/model/player.py
@@ -0,0 +1,63 @@
+import hashlib
+
+from twisted.internet import defer
+
+from xo_server.common.singletone import service
+
+
+def calc_password_hash(password):
+    #TODO: use more robust security schema using
+    #separate salt for each user
+    data = "{}{}".format(password, service.config["server_salt"])
+    return hashlib.sha1(data).hexdigest()
+
+
+
+class Player(object):
+    table_name = "players"
+
+    def __init__(self, nickname=None, xp=0, email=None,
+                password=None):
+        self.nickname = nickname
+        self.xp = xp
+        self.email = email
+        self.password = password
+        self.password_hash = None
+        self.id = None
+
+
+    @defer.inlineCallbacks
+    def is_exists(self, email=None):
+        yield 1
+        defer.returnValue(False)
+
+    
+
+    @defer.inlineCallbacks
+    def insert(self):
+        if self.password_hash is None:
+            self.password_hash = calc_password_hash(self.password)
+
+        sql_cmd = """INSERT INTO players (nickname, xp, email, 
+                     password_hash, created, updated) VALUES
+                     (%(nickname)s, %(xp)s, %(email)s, %(password_hash)s,
+                     utc_timestamp(), utc_timestamp())"""
+        sql_data = {
+            "nickname": self.nickname,
+            "email": self.email,
+            "xp": self.xp,
+            "password_hash": self.password_hash,
+        }
+        player_id = yield service.sql_db.runInteraction(sql_cmd, sql_data)
+        self.id = player_id
+
+
+
+    @defer.inlineCallbacks
+    def update(self):
+        yield 1
+
+
+    @defer.inlineCallbacks
+    def load_by_email(self, email):
+        yield 1
---
+++ xo_server/model/session.py
@@ -0,0 +1,47 @@
+import hashlib
+import datetime
+
+from twisted.internet import defer
+
+from xo_server.common.singletone import service
+
+
+class Session(object):
+    table_name = "sessions"
+
+    def __init__(self, player_id=None, ip_addr=None):
+        self.player_id = player_id
+        self.ip_addr = ip_addr
+        self.sid = None
+        self.id = None
+
+    def generate_sid(self):
+        sha1_object = hashlib.sha1("{}{}{}".format(
+                            self.player_id, 
+                            datetime.datetime.now(),
+                            service.config["server_salt"])
+                      )
+        return sha1_object.hexdigest()
+
+
+    @defer.inlineCallbacks
+    def insert(self):
+        if self.sid is None:
+            self.sid = self.generate_sid()
+
+        sql_cmd = """INSERT INTO sessions (player_id, sid, ip_addr,
+                     created) VALUES
+                     (%(player_id)s, %(sid)s, %(ip_addr)s, utc_timestamp())"""
+        sql_data = {
+            "player_id": self.player_id,
+            "sid": self.sid,
+            "ip_addr": self.ip_addr,
+        }
+        player_id = yield service.sql_db.runInteraction(sql_cmd, sql_data)
+        self.id = player_id
+
+
+    @defer.inlineCallbacks
+    def load_by_sid(self, sid):
+        yield 1
+
