e49140de1f7e92b6eb02ee69005207e83d72b574
Alexei Romanoff <drednout.by@gmail.com>
2015-01-08 09:20:18 +0300

    Add serialization adapter initializing

--- xo_server/common/singletone.py
+++ xo_server/common/singletone.py
@@ -1,12 +1,14 @@
 from twisted.internet import defer
 
 import xo_server.common.sql_db as sql_db
+import xo_server.common.serialization_adapter as serialization_adapter
 
 
 class ServiceSingletone(object):
     def __init__(self):
         self.name = None
         self.config = None
+        self.serialization_adapter = None
 
     @defer.inlineCallbacks
     def initialize(self):
@@ -15,10 +17,21 @@ class ServiceSingletone(object):
         #db ping
         yield service.sql_db.runQuery("SELECT 1")
 
+        serialization_format = self.config["serialization_format"]
+        self.serialization_adapter = serialization_adapter.getSerialiazationAdapter(serialization_format)
+
 
     @defer.inlineCallbacks
     def finalize(self):
         yield 1
+
+
+    def pack(self, msg):
+        return self.serialization_adapter.pack(msg)
+
+
+    def unpack(self, msg):
+        return self.serialization_adapter.unpack(msg)
 
 
 
